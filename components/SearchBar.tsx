80b1db4f67ec72b44d01873fa33de91db89046f6
TDMwangi <teddymwangi65@gmail.com>
2026-02-08 11:29:00 +0300

    added a custom hook

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,26 +1,30 @@
-import { Image, TextInput, View } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { TextInput, View } from 'react-native';
 
-import { icons } from '@/constants/icons';
-
-interface SearchProps {
-  onPress?: () => void;
+interface SearchInputProps {
   placeholder: string;
+  onPress?: () => void;
+  value?: string;
+  onChangeText?: (text: string) => void;
 }
 
-const SearchBar = ({ onPress, placeholder }: SearchProps) => {
+export default function SearchInput({
+  placeholder,
+  onPress,
+  value,
+  onChangeText,
+}: SearchInputProps) {
   return (
-    <View className="flex-row items-center bg-black rounded-full px-5 py-4">
-      <Image source={icons.search} className="size-5" resizeMode="contain" tintColor="#ab8bff" />
+    <View className="flex-row items-center bg-neutral-800 rounded-full px-4 py-3 border border-neutral-700">
+      <Feather name="search" size={18} color="#e5e7eb" />
       <TextInput
-        onPress={onPress}
         placeholder={placeholder}
-        value=""
-        onChangeText={() => { }}
-        placeholderTextColor="#a8b5db"
-        className="flex-1 ml-2 text-white"
+        onPress={onPress}
+        value={value}
+        onChangeText={onChangeText}
+        placeholderTextColor="#e5e7eb"
+        className="flex-1 ml-3 text-white text-base"
       />
     </View>
   );
-};
-
-export default SearchBar;
+}
