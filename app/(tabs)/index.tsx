9aa73cb7bd7eef0252895609ca2afb19cbfd3bf4
Teddy D. Mwangi <57321721+TDMwangi@users.noreply.github.com>
2026-02-08 11:34:37 +0300

    added a custom hook

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,25 +1,77 @@
 import { useRouter } from 'expo-router';
-import { Image, ScrollView, View } from 'react-native';
+import { ActivityIndicator, FlatList, Image, Text, View } from 'react-native';
 
+import MovieCard from '@/components/MovieCard';
 import SearchBar from '@/components/SearchBar';
 import { icons } from '@/constants/icons';
-import { images } from '@/constants/images';
+import { fetchMovies } from '@/services/api';
+import useFetch from '@/services/useFetch';
 
 export default function Index() {
   const router = useRouter();
 
+  const {
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError
+  } = useFetch(() => fetchMovies({
+    query: '',
+  }));
+
+  if (moviesLoading) {
+    return (
+      <View className="flex-1 justify-center items-center">
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
+  }
+
+  if (moviesError) {
+    return (
+      <View className="flex-1 justify-center items-center">
+        <Text>
+          Error: {moviesError.message}
+        </Text>
+      </View>
+    );
+  }
+
   return (
     <View className="flex-1">
-      <Image source={images.bg} className="absolute w-full z-0" />
-      <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}>
-        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
-        <View className="flex-1 mt-5">
-          <SearchBar
-            onPress={() => router.push("/search")}
-            placeholder="Search for a movie"
-          />
-        </View>
-      </ScrollView>
+      <FlatList
+        data={movies}
+        keyExtractor={(item) => item.id.toString()}
+        numColumns={3}
+        className="mt-2 pb-32"
+        scrollEnabled={true}
+        columnWrapperStyle={{
+          justifyContent: 'space-between',
+          paddingHorizontal: 8,
+        }}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingTop: 8 }}
+        ListHeaderComponent={
+          <View className="bg-neutral-950 pt-20 pb-6 px-5 border-b border-neutral-800">
+            <Image
+              source={icons.logo}
+              className="w-12 h-10 mb-4 mx-auto"
+              resizeMode="contain"
+            />
+
+            <SearchBar
+              placeholder="Search movies"
+              onPress={() => router.push('/search')}
+            />
+
+            <View className="px-5 mt-6 mb-3">
+              <Text className="text-lg text-white font-bold">
+                Latest Movies
+              </Text>
+            </View>
+          </View>
+        }
+        renderItem={({ item }) => <MovieCard {...item} />}
+      />
     </View>
   );
 }
--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,11 +1,86 @@
-import { Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { ActivityIndicator, FlatList, Image, Text, View } from 'react-native';
 
-const Search = () => {
+import MovieCard from '@/components/MovieCard';
+import SearchBar from '@/components/SearchBar';
+import { icons } from '@/constants/icons';
+import { fetchMovies } from '@/services/api';
+import { updateSearchCount } from '@/services/appwrite';
+import useFetch from '@/services/useFetch';
+
+export default function Index() {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const {
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError,
+    refetch: loadMovies,
+    reset
+  } = useFetch(() => fetchMovies({
+    query: searchQuery,
+  }), false);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(async () => {
+      if (searchQuery.trim()) {
+        await loadMovies();
+
+        if (movies?.length > 0 && movies?.[0]) {
+          updateSearchCount(searchQuery, movies[0]);
+        }
+      } else {
+        reset();
+      }
+    }, 500);
+
+    return () => clearTimeout(timeoutId);
+  }, [searchQuery]);
+
   return (
-    <View>
-      <Text>Search</Text>
+    <View className="flex-1">
+      <FlatList
+        data={movies}
+        keyExtractor={(item) => item.id.toString()}
+        numColumns={3}
+        className="mt-2 pb-32"
+        scrollEnabled={true}
+        columnWrapperStyle={{
+          justifyContent: 'space-between',
+          paddingHorizontal: 8,
+        }}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingTop: 8 }}
+        ListHeaderComponent={
+          <View className="bg-neutral-950 pt-20 pb-6 px-5 border-b border-neutral-800">
+            <Image
+              source={icons.logo}
+              className="w-12 h-10 mb-4 mx-auto"
+              resizeMode="contain"
+            />
+
+            <SearchBar
+              placeholder="Search movies"
+              value={searchQuery}
+              onChangeText={(text: string) => setSearchQuery(text)}
+            />
+
+            {moviesLoading && (
+              <ActivityIndicator size="large" color="#0000ff" className="my-3" />
+            )}
+
+            {
+              !moviesLoading && !moviesError && searchQuery.trim() !== '' && (
+                <Text className="text-xl text-white mt-3 font-bold">
+                  Search Results for{' '}
+                  <Text className="text-accent">{searchQuery}</Text>
+                </Text>
+              )
+            }
+          </View>
+        }
+        renderItem={({ item }) => <MovieCard {...item} />}
+      />
     </View>
   );
-};
-
-export default Search;
+}
